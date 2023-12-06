3b0197acbe7348aa316124870d7d711060c04b66
pingstanton <108839493+pingstanton@users.noreply.github.com>
2023-12-06 10:28:35 -0500

    Update nypdmap.js
    Overwrote with final changes.

--- nypdmap.js
+++ nypdmap.js
@@ -1,10 +1,12 @@
 let nycMap;
 nycMap = L.map("map");
 
-// create tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// swap out styles from this gallery:
+// https://leaflet-extras.github.io/leaflet-providers/preview/
+
+L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 16,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
 }).addTo(nycMap);
 
 nycMap.setView([40.7130, -73.980], 11);
@@ -26,7 +28,7 @@ L.geoJSON(nyc, {
 //adds popup info to each neighborhood
 L.geoJSON(nyc, {
     onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.borough + "</h3> <hr> <h3>Police Precinct " + feature.properties.policePrecinct + "</h3>");
+        layer.bindPopup("<h3>Police Precinct #" + feature.properties.policePrecinct + "</h3><h4>Borough: " + feature.properties.borough + "</h4> <hr> <table cellspacing='0' cellpadding='0' border='0'><tr><td>&nbsp;</td><td><strong>'18</strong></td><td><strong>'19</strong></td><td><strong>'20</strong></td><td><strong>'21</strong></td><td><strong>'22</strong></td><td><strong>Total</strong></td></tr><tr><td class='t'><strong>Murder</strong></td><td>" + feature.properties.murder18 + "</td><td>" + feature.properties.murder19 + "</td><td>" + feature.properties.murder20 + "</td><td>" + feature.properties.murder21 + "</td><td>" + feature.properties.murder22 + "</td><td><strong>" + feature.properties.murderT + "</strong></td></tr><tr><td class='t'><strong>Rape</strong></td><td>" + feature.properties.rape18 + "</td><td>" + feature.properties.rape19 + "</td><td>" + feature.properties.rape20 + "</td><td>" + feature.properties.rape21 + "</td><td>" + feature.properties.rape22 + "</td><td><strong>" + feature.properties.rapeT + "</strong></td></tr><tr><td class='t'><strong>Robbery</strong></td><td>" + feature.properties.robbery18 + "</td><td>" + feature.properties.robbery19 + "</td><td>" + feature.properties.robbery20 + "</td><td>" + feature.properties.robbery21 + "</td><td>" + feature.properties.robbery22 + "</td><td><strong>" + feature.properties.robberyT + "</strong></td></tr><tr><td  class='t'><strong>Assault</strong></td><td>" + feature.properties.assault18 + "</td><td>" + feature.properties.assault19 + "</td><td>" + feature.properties.assault20 + "</td><td>" + feature.properties.assault21 + "</td><td>" + feature.properties.assault22 + "</td><td><strong>" + feature.properties.assaultT + "</strong></td></tr><tr><td  class='t'><strong>Burglary</strong></td><td>" + feature.properties.burglary18 + "</td><td>" + feature.properties.burglary19 + "</td><td>" + feature.properties.burglary20 + "</td><td>" + feature.properties.burglary21 + "</td><td>" + feature.properties.burglary22 + "</td><td><strong>" + feature.properties.burglaryT + "</strong></td></tr><tr><td class='t'><strong>G.&nbsp;Larceny</strong></td><td>" + feature.properties.larceny18 + "</td><td>" + feature.properties.larceny19 + "</td><td>" + feature.properties.larceny20 + "</td><td>" + feature.properties.larceny21 + "</td><td>" + feature.properties.larceny22 + "</td><td><strong>" + feature.properties.larcenyT + "</strong></td></tr><tr><td class='t'><strong>G.&nbsp;L.&nbsp;Auto</strong></td><td>" + feature.properties.auto18 + "</td><td>" + feature.properties.auto19 + "</td><td>" + feature.properties.auto20 + "</td><td>" + feature.properties.auto21 + "</td><td>" + feature.properties.auto22 + "</td><td><strong>" + feature.properties.autoT + "</strong></td></tr></table>");
     }
 }).addTo(nycMap);
 
